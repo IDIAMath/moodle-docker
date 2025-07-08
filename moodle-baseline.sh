deef8565aec3c3df9abc173d4dce7820cd3a2db3
Hans Georg Schaathun <georg@schaathun.net>
2025-07-08 09:18:18 +0200

    baseline-script

---
+++ moodle-baseline.sh
@@ -0,0 +1,23 @@
+export MOODLE_DOCKER_WWWROOT=../moodle-baseline
+export MOODLE_DOCKER_DB=pgsql
+export MOODLE_DOCKER_WEB_HOST=`hostname`
+export MOODLE_DOCKER_PHP_VERSION=8.0
+
+export COMPOSE_PROJECT_NAME=moodle-baseline
+export MOODLE_DOCKER_WEB_PORT=0.0.0.0:8888
+
+
+# Ensure customized config.php for the Docker containers is in place
+\cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+
+# Build the docker image with YAML
+sh build.sh
+
+# Start up containers
+bin/moodle-docker-compose up -d
+
+# Wait for DB to come up (important for oracle/mssql)
+bin/moodle-docker-wait-for-db
+
+# Initialize Moodle database for manual testing
+bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="Docker moodle" --shortname="docker_moodle" --summary="Docker moodle site" --adminpass="test" --adminemail="admin@example.com"
